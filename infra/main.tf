6777c990908a534eecdd37521866798de74e58cb
Praveen B <praveenpraveen8407@gmail.com>
2025-12-14 19:31:09 +0530

    Add more insecure resources

--- infra/main.tf
+++ infra/main.tf
@@ -17,3 +17,27 @@ resource "aws_iam_policy" "weak_policy" {
     }]
   })
 }
+
+# IAM Policy - wildcard EC2 (HIGH)
+resource "aws_iam_policy" "wild_ec2_policy" {
+  name = "wild-ec2-policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "ec2:*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+# Public write bucket (CRITICAL)
+resource "aws_s3_bucket" "public_write_bucket" {
+  bucket = "public-write-bucket"
+  acl    = "public-read-write"
+}
+
